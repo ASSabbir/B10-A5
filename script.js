afc995e30825ad30047533359594141db9a5631a
ASSabbir <atiksabbir1125@gmail.com>
2024-09-24 03:00:42 +0600

    added some section

--- script.js
+++ script.js
@@ -1,17 +1,35 @@
+function calculate(price, money,parent) {
+    const number = /\d/;
+    const string = /[a-zA-Z]/;
+    if (number.test(money) && string.test(money)) {
+        alert('this is not money')
+    }
+    else if (number.test(money)) {
+        const temp=parseInt(price.innerText);
+        price.innerText=parseInt(money)+temp;
+        const myAccount=document.querySelector('#total-Account');
+        myAccount.innerText=myAccount.innerText-money;
+        const output=document.querySelector('#output')
+        const date=new Date()
+        
+        const h1=parent.children[1].innerText;
+        console.log(h1)
+        output.innerHTML=`<h1 class="text-xl font-bold grotesk "> ${money} Taka is ${h1}</h1> 
+                <p class="text-zinc-400">Date : ${date.toDateString()}</p>`
+        
+    }
+    else if (string.test(money)) {
+        alert('this is not money')
+    }
+
+}
 
 
-document.querySelector('#donet-btn').addEventListener('click', function () {
-    const donetion = parseInt(document.querySelector('#price span').innerText);
+document.querySelector('#donet-btn').addEventListener('click', function (event) {
+    const donetion = document.querySelector('#price span')
     const inputField = document.querySelector('.card-text input').value;
-    const number=/\d/;
-    const string=/[a-zA-Z]/;
-    if(number.test(inputField) && string.test(inputField)){
-       
-    }
-    else if(number.test(inputField)){
-        alert('this is not money')
-    }
-    else if(string.test(inputField)){
-        alert('this is not money')
-    }
+    const parent=event.target.parentElement.parentElement
+    calculate(donetion, inputField,parent)
+    
 })
+
