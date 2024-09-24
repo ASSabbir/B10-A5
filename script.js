16d4b5a282668cdd7d8e4448a7d8783a59d62558
ASSabbir <atiksabbir1125@gmail.com>
2024-09-24 15:06:16 +0600

    added fuctionality

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 function calculate(price, money,parent) {
     const number = /\d/;
     const string = /[a-zA-Z]/;
+    
     if (number.test(money) && string.test(money)) {
         alert('this is not money')
     }
@@ -13,9 +14,13 @@ function calculate(price, money,parent) {
         const date=new Date()
         
         const h1=parent.children[1].innerText;
-        console.log(h1)
-        output.innerHTML=`<h1 class="text-xl font-bold grotesk "> ${money} Taka is ${h1}</h1> 
-                <p class="text-zinc-400">Date : ${date.toDateString()}</p>`
+        
+        let apent=document.createElement('div')
+        apent.innerHTML =`<div class="border-[1px] border-zinc-600 p-5 rounded-2xl">
+            <h1 class="text-xl font-bold grotesk "> ${money} Taka is ${h1}</h1> 
+            <p class="text-zinc-400">Date : ${date.toDateString()}</p>
+        </div>`;
+        output.appendChild(apent)
         
     }
     else if (string.test(money)) {
@@ -25,9 +30,30 @@ function calculate(price, money,parent) {
 }
 
 
-document.querySelector('#donet-btn').addEventListener('click', function (event) {
+const btn1= document.querySelector('#donet-btn')
+btn1.addEventListener('click', function (event) {
     const donetion = document.querySelector('#price span')
     const inputField = document.querySelector('.card-text input').value;
+    const inputFieldcontainer = document.querySelector('.card-text input');
+    inputFieldcontainer.value=''
+    const parent=event.target.parentElement.parentElement;
+    calculate(donetion, inputField,parent)
+    
+})
+document.querySelector('#donet-btn2').addEventListener('click', function (event) {
+    const donetion = document.querySelector('#price2 span')
+    const inputField = document.querySelector('.card-text2 input').value;
+    const inputFieldcontainer2 = document.querySelector('.card-text2 input');
+    inputFieldcontainer2.value=''
+    const parent=event.target.parentElement.parentElement
+    calculate(donetion, inputField,parent)
+    
+})
+document.querySelector('#donet-btn3').addEventListener('click', function (event) {
+    const donetion = document.querySelector('#price3 span')
+    const inputField = document.querySelector('.card-text3 input').value;
+    const inputFieldcontainer2 = document.querySelector('.card-text3 input');
+    inputFieldcontainer2.value=''
     const parent=event.target.parentElement.parentElement
     calculate(donetion, inputField,parent)
     
